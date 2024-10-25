855ca4306a6151a8818101f3462cddd7091e048c
N angular <nangular04@gmail.com>
2024-10-25 08:26:38 -0500

    ADD multiplicar y dividir

--- src/com/miempresa/miapp/App.java
+++ src/com/miempresa/miapp/App.java
@@ -9,4 +9,12 @@ public class App {
 	public static double restar(double a, double b) {
 		return a - b;
 	}
+	
+	public static double multiplicar(double a, double b) {
+		return a * b;
+	}
+	
+	public static double dividir(double a, double b) {
+		return a / b;
+	}
 }
